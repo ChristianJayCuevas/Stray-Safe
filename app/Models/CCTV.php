adbf0245f09785f3298addcd6c8133a0ff23c5d9
ChristianJayCuevas <christianjaycuevas@gmail.com>
2025-03-26 21:21:24 +0800

    Added CCTV Table, Created CCTV Cards

--- app/Models/CCTV.php
+++ app/Models/CCTV.php
@@ -10,6 +10,13 @@ class CCTV extends Model
     use HasFactory;
     
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cctvs';
+    
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
